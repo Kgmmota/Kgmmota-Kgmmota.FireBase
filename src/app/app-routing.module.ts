ba8c28635f8ee5f16e4eb8ade606f590920779fc
Kgmmota <motagui2000@outlook.com>
2026-02-02 12:32:27 -0300

    add login with routing and basic template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [{
-  path:'', component: LoginComponent
-}, ];
+  path:'', component: LoginComponent}, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

44f1533e29e5c5b386c47af772068b765479bd60
Kgmmota <motagui2000@outlook.com>
2026-02-02 12:50:09 -0300

    integração angular/firebase

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyAqHVoaiF658aSlMnpLlw4pRSEwrxjFDPM",
+        authDomain: "asimovies-f61f2.firebaseapp.com",
+        projectId: "asimovies-f61f2",
+        storageBucket: "asimovies-f61f2.firebasestorage.app",
+        messagingSenderId: "1006346579650",
+        appId: "1:1006346579650:web:73a6b35d80f61aa87ad55b",
+        measurementId: "G-LGL24NLQ85"
+    }
+}

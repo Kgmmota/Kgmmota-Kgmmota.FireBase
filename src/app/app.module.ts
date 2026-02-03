b5f4d99008eb76428c77919d60c2e60b31ea1aa4
Kgmmota <motagui2000@outlook.com>
2026-02-03 10:11:04 -0300

    Multiplas Configurações

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from './environments/environment';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { RecuperarSenhaComponent } from './components/recuperar-senha/recuperar-senha.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    CadastroComponent,
+    RecuperarSenhaComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

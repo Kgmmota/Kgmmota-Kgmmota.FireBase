1c12c14f9f9242dec103ab07657dd24400ae48d8
Kgmmota <motagui2000@outlook.com>
2026-02-03 12:08:33 -0300

    logica de autentificação

--- src/app/components/cadastro/cadastro.component.ts
+++ src/app/components/cadastro/cadastro.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../shared/services/auth.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -11,5 +12,22 @@ export class CadastroComponent {
   email: string ='';
   password: string = '';
   confirmPassword: string = '';
+
+  constructor(private auth: AuthService){}
+
+  valideForm(): boolean{
+    return this.name !== '' &&
+           this.email !== '' &&
+           this.password !== '' &&
+           this.confirmPassword !== '';
+  }
+
+  cadastrar(){
+    if(this.valideForm()){
+      this.auth.cadastro(this.name,this.email,this.password,this.confirmPassword)
+    }else{
+      alert('Preencha todos os Campos.')
+    }
+  }
   
 }
